6fdce009a9229175fdd47f690aba9f867bf168fd
doodledoodle128-prog <doodle.doodle128@gmail.com>
2026-03-06 16:25:07 -0500

    Add files via upload

---
+++ And_or_activity.py
@@ -0,0 +1,13 @@
+a = 0
+b = 0
+c = 0
+
+if a and b and c:
+    print("All of the numbers are positive")
+else:
+    print("One of the numbers are negitive")
+
+if a or b or c:
+    print("One one the numbers are positive")
+else:
+    print("all are negitive")
