182a37435f7c1f33637b127c4590d9467ea48c63
doodledoodle128-prog <doodle.doodle128@gmail.com>
2026-03-06 16:39:54 -0500

    Add files via upload

---
+++ not_equal.py
@@ -0,0 +1,24 @@
+a = 3
+b = 2
+c = 7
+
+print(a != b)
+print(b != c)
+
+a = 'add'
+b = 'sub'
+
+if a != b:
+    print("'a' and 'b' are different")
+
+a = 10
+b = 19
+
+if (a==1) != (b==19):
+    print("hello")
+
+num = int(input('what is number '))
+if num%2 != 0:
+    print("not even")
+else:
+    print('even')
